79ba72d654a8ac75faa60e4ccbdb483a2ff21c3e
Dinh Hoang Long <dinhhoanglong@gmail.com>
2025-05-28 11:35:18 +0700

    update
    update

--- Backend/RIPT1307-BTL/Common/Room.cs
+++ Backend/RIPT1307-BTL/Common/Room.cs
@@ -8,12 +8,13 @@ namespace RIPT1307_BTL.Common
     {
         public int RoomID { get; set; }              // RoomID INT AUTO_INCREMENT PRIMARY KEY
         public string RoomName { get; set; }         // RoomName VARCHAR(50) NOT NULL
-        public string RoomType { get; set; }         // RoomType ENUM('Single', 'Double') NOT NULL
+        public string BaseRoomType { get; set; }         // RoomType ENUM('Single', 'Double') NOT NULL
         public decimal Price { get; set; }            // Price DECIMAL(10,2) NOT NULL
         public string Status { get; set; }            // Status ENUM('Available', 'In Use','Being Cleaned', 'Under Maintenance', 'Reserved') DEFAULT 'Available'
         public string? Description { get; set; }     // Description TEXT (có thể null)
         public int RoomTypeID { get; set; }              // Foreign key
 
+        public RoomType RoomType { get; set; }
         public List<RoomService> RoomServices { get; set; }
 
     }
